b267dc4d00a60f4650e4534ff77fd95c8dd4d5e6
Larry Adams <thewitness@cacti.net>
2010-05-16 13:28:08 +0000

    QA: Change Pagination function in syslog.php QA: Removing deprecated functions
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/syslog/trunk@911 67214365-cc29-0410-bfbe-843132f0f3d7

--- syslog.php
+++ syslog.php
@@ -635,38 +635,41 @@ function syslog_messages() {
 
 	$total_rows = syslog_filter($sql_where);
 
-	$nav = "<tr bgcolor='#" . $colors["header"] . "'>
-		<td colspan='9'>
-			<table width='100%' cellspacing='0' cellpadding='0' border='0'>
-				<tr>
-					<td align='left' class='textHeaderDark'>
-						<strong>&lt;&lt; ";
-						if (isset($_REQUEST["page"]) && $_REQUEST["page"] > 1) {
-							$nav .= "<a class='linkOverDark' href='" . get_query_edited_url($url_curr_page, 'page', ($_REQUEST["page"]-1)) . "'>";
-						}
-						$nav .= "Previous";
-						if (isset($_REQUEST["page"]) && $_REQUEST["page"] > 1) {
-							$nav .= "</a>";
-						}
-						$nav .= "</strong>
-					</td>\n
-					<td align='center' class='textHeaderDark'>
-						Showing Rows " . (($_REQUEST["rows"]*($_REQUEST["page"]-1))+1) . " to " . ((($total_rows < $_REQUEST["rows"]) || ($total_rows < ($_REQUEST["rows"]*$_REQUEST["page"]))) ? $total_rows : ($_REQUEST["rows"]*$_REQUEST["page"])) . " of $total_rows [ " . trim(syslog_page_select($total_rows)) . " ]
-					</td>\n
-					<td align='right' class='textHeaderDark'><strong>";
-						if (isset($_REQUEST["page"]) && ($_REQUEST["page"] * $_REQUEST["rows"]) < $total_rows) {
-							$nav .= "<a class='linkOverDark' href='" . get_query_edited_url($url_curr_page, 'page', ($_REQUEST["page"]+1)) . "'>";
-						}
-						$nav .= "Next";
-						if (($_REQUEST["page"] * $_REQUEST["rows"]) < $total_rows) {
-							$nav .= "</a>";
-						}
-						$nav .= " &gt;&gt;</strong>
-					</td>\n
-				</tr>
-			</table>
-		</td>
-	</tr>\n";
+	/* generate page list */
+	$url_page_select = get_page_list($_REQUEST["page"], MAX_DISPLAY_PAGES, $_REQUEST["rows"], $total_rows, "syslog.php");
+
+	if ($total_rows > 0) {
+		$nav = "<tr bgcolor='#" . $colors["header"] . "'>
+					<td colspan='13'>
+						<table width='100%' cellspacing='0' cellpadding='0' border='0'>
+							<tr>
+								<td align='left' class='textHeaderDark'>
+									<strong>&lt;&lt; "; if ($_REQUEST["page"] > 1) { $nav .= "<a class='linkOverDark' href='syslog.php.php?report=arp&page=" . ($_REQUEST["page"]-1) . "'>"; } $nav .= "Previous"; if ($_REQUEST["page"] > 1) { $nav .= "</a>"; } $nav .= "</strong>
+								</td>\n
+								<td align='center' class='textHeaderDark'>
+									Showing Rows " . ($total_rows == 0 ? "None" : (($row_limit*($_REQUEST["page"]-1))+1) . " to " . ((($total_rows < $row_limit) || ($total_rows < ($row_limit*$_REQUEST["page"]))) ? $total_rows : ($row_limit*$_REQUEST["page"])) . " of $total_rows [$url_page_select]") . "
+								</td>\n
+								<td align='right' class='textHeaderDark'>
+									<strong>"; if (($_REQUEST["page"] * $row_limit) < $total_rows) { $nav .= "<a class='linkOverDark' href='syslog.php.php?report=arp&page=" . ($_REQUEST["page"]+1) . "'>"; } $nav .= "Next"; if (($_REQUEST["page"] * $row_limit) < $total_rows) { $nav .= "</a>"; } $nav .= " &gt;&gt;</strong>
+								</td>\n
+							</tr>
+						</table>
+					</td>
+				</tr>\n";
+	}else{
+		$nav = "<tr bgcolor='#" . $colors["header"] . "' class='noprint'>
+					<td colspan='22'>
+						<table width='100%' cellspacing='0' cellpadding='0' border='0'>
+							<tr>
+								<td align='center' class='textHeaderDark'>
+									No Rows Found
+								</td>\n
+							</tr>
+						</table>
+					</td>
+				</tr>\n";
+	}
+
 	print $nav;
 
 	$display_text = array(
