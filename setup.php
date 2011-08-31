938f1007bd206681c686e79ed43dfda19ad2c912
Larry Adams <thewitness@cacti.net>
2011-08-31 19:34:56 +0000

    Making new release prior to i18n
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/syslog/trunk@1629 67214365-cc29-0410-bfbe-843132f0f3d7

--- setup.php
+++ setup.php
@@ -818,7 +818,7 @@ function syslog_setup_table_new($options) {
 function syslog_version () {
 	return array(
 		'name'     => 'syslog',
-		'version'  => '1.21',
+		'version'  => '1.22',
 		'longname' => 'Syslog Monitoring',
 		'author'   => 'Jimmy Conner',
 		'homepage' => 'http://cactiusers.org',
