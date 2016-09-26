d9443a69ad08dc2b667fd906a1422614bff67342
cigamit <jimmy@sqmail.org>
2016-09-26 15:24:59 -0500

    Use INFO files for plugins

--- setup.php
+++ setup.php
@@ -1,16 +1,27 @@
 <?php
-/*******************************************************************************
+/*
  ex: set tabstop=4 shiftwidth=4 autoindent:
-
-    Author ......... Jimmy Conner
-    Contact ........ jimmy@sqmail.org
-    Home Site ...... http://cactiusers.org
-    Program ........ Syslog Plugin for Cacti
-    Purpose ........ Sylog viewer for cacti
-    Originally released as aloe by: sidewinder <sidewinder@shitworks.com>
-    Modified by: Harlequin harlequin@cyberonic.com> as h.aloe
-
-*******************************************************************************/
+ +-------------------------------------------------------------------------+
+ | Copyright (C) 2004-2016 The Cacti Group                                 |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+ | Cacti: The Complete RRDTool-based Graphing Solution                     |
+ +-------------------------------------------------------------------------+
+ | This code is designed, written, and maintained by the Cacti Group. See  |
+ | about.php and/or the AUTHORS file for specific developer information.   |
+ +-------------------------------------------------------------------------+
+ | http://www.cacti.net/                                                   |
+ +-------------------------------------------------------------------------+
+*/
 
 include_once($config['base_path'] . '/plugins/syslog/database.php');
 
@@ -461,15 +472,9 @@ function syslog_setup_table_new($options) {
 }
 
 function plugin_syslog_version () {
-	return array(
-		'name'     => 'syslog',
-		'version'  => '2.0',
-		'longname' => 'Syslog Monitoring',
-		'author'   => 'Jimmy Conner',
-		'homepage' => 'http://cactiusers.org',
-		'email'    => 'jimmy@sqmail.org',
-		'url'      => 'http://versions.cactiusers.org/'
-	);
+	global $config;
+	$info = parse_ini_file($config['base_path'] . '/plugins/syslog/INFO', true);
+	return $info['info'];
 }
 
 function syslog_check_dependencies() {
