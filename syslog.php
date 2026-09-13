932d0a69b497eaa2487d2ef0df471fcd2f24d783
Sean Mancini <sean@seanmancini.com>
2026-09-12 21:06:00 -0400

    adjust dropdwn filter displays

--- syslog.php
+++ syslog.php
@@ -1347,6 +1347,8 @@ function syslog_filter($sql_where, $tab) {
 									?>
 								</select>
 							</td>
+						</tr>
+						<tr>
 							<td>
 								<?php print __('Trim', 'syslog'); ?>
 							</td>
@@ -1365,6 +1367,8 @@ function syslog_filter($sql_where, $tab) {
 									?>
 								</select>
 							</td>
+						</tr>
+						<tr>
 							<td>
 								<?php print __('Refresh', 'syslog'); ?>
 							</td>
@@ -1384,11 +1388,11 @@ function syslog_filter($sql_where, $tab) {
 								</select>
 							</td>
 						</tr>
-					</table>
-					<table class='filterTable syslogSearchFilters'>
 						<tr>
 							<?php api_plugin_hook('syslog_extend_filter'); ?>
-							<?php if (get_nfilter_request_var('tab') == 'syslog') { ?>
+						</tr>
+						<?php if (get_nfilter_request_var('tab') == 'syslog') { ?>
+						<tr>
 							<td>
 								<?php print __('Record Type', 'syslog'); ?>
 							</td>
@@ -1399,10 +1403,12 @@ function syslog_filter($sql_where, $tab) {
 									<option value='2'<?php if (get_request_var('removal') == '2') { ?> selected<?php } ?>><?php print __('Removed Records', 'syslog'); ?></option>
 								</select>
 							</td>
-							<?php } else { ?>
-							<input type='hidden' id='removal' value='<?php print html_escape_request_var('removal'); ?>'>
-							<?php } ?>
-							<?php if (get_nfilter_request_var('tab') == 'syslog') { ?>
+						</tr>
+						<?php } else { ?>
+						<input type='hidden' id='removal' value='<?php print html_escape_request_var('removal'); ?>'>
+						<?php } ?>
+						<?php if (get_nfilter_request_var('tab') == 'syslog') { ?>
+						<tr>
 							<td>
 								<?php print __('Display', 'syslog'); ?>
 							</td>
@@ -1412,10 +1418,10 @@ function syslog_filter($sql_where, $tab) {
 									<option value='1'<?php if (get_request_var('grouping') == '1') { ?> selected<?php } ?>><?php print __('Grouped Messages', 'syslog'); ?></option>
 								</select>
 							</td>
-							<?php } else { ?>
-							<input type='hidden' id='grouping' value='0'>
-							<?php } ?>
 						</tr>
+						<?php } else { ?>
+						<input type='hidden' id='grouping' value='0'>
+						<?php } ?>
 					</table>
 					<div class='syslogSearchFooter'>
 						<details id='logical_search_help'><summary><?php print __esc('Search help', 'syslog'); ?></summary><?php print __esc('Choose a field, operator, and value. AND takes precedence over OR; NOT excludes a condition. LIKE uses % for any number of characters and _ for one character. Dates use YYYY-MM-DD HH:MM:SS. IDs use nonnegative integers.', 'syslog'); ?></details>
