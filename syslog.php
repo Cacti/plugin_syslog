6c43c0340c6d340f43f0f252c85249b389e25933
Sean Mancini <sean@seanmancini.com>
2026-09-13 20:01:49 -0400

    adjust styling for filtering

--- syslog.php
+++ syslog.php
@@ -1913,13 +1913,13 @@ function syslog_messages($tab = 'syslog') {
 					form_selectable_cell($sm['logtime'], $sm['seq'], '', 'left');
 				}
 
-				form_selectable_cell(syslog_value_filter_button($hosts[$sm['host_id']] ?? __('Unknown', 'syslog'), 'host'), $sm['seq'], '', 'left syslogMeta');
-				form_selectable_cell(syslog_value_filter_button($sm['program'], 'program'), $sm['seq'], '', 'left syslogMeta');
+				print "<td class='nowrap left syslogMeta'>" . syslog_value_filter_button($hosts[$sm['host_id']] ?? __('Unknown', 'syslog'), 'host') . '</td>';
+				print "<td class='nowrap left syslogMeta'>" . syslog_value_filter_button($sm['program'], 'program') . '</td>';
 				// Group summaries show the latest timestamp; use its matching sequence ID.
 				$rule_id = $grouping_enabled && !empty($sm['seq_list']) ? explode(',', $sm['seq_list'])[0] : $sm[$syslog_incoming_config['id']];
 				form_selectable_cell(syslog_message_button($sm['message'], $hosts[$sm['host_id']] ?? '', $sm['program'], $facilities[$sm['facility_id']] ?? '', $priorities[$sm['priority_id']] ?? '', $sm['logtime'], $rule_id, $sm['mtype']), $sm['seq'], '', 'left syslogMessage');
 				form_selectable_cell(syslog_metadata_label(isset($facilities[$sm['facility_id']]) ? $facilities[$sm['facility_id']] : __('Unknown', 'syslog'), 'facility'), $sm['seq'], '', 'left');
-				form_selectable_cell(syslog_metadata_label(isset($priorities[$sm['priority_id']]) ? $priorities[$sm['priority_id']] : __('Unknown', 'syslog'), 'priority'), $sm['seq'], '', 'left');
+				form_selectable_cell(syslog_value_filter_button($priorities[$sm['priority_id']] ?? __('Unknown', 'syslog'), 'priority'), $sm['seq'], '', 'left');
 
 				// Add occurrence count if grouping is enabled
 				if ($grouping_enabled) {
@@ -1946,11 +1946,11 @@ function syslog_messages($tab = 'syslog') {
 
 
 							print "<td class='left' style='padding-left:30px;'>" . html_escape($dm['logtime'], $dm[$syslog_incoming_config['id']], $sm['mtype']) . '</td>';
-							print "<td class='left'>" . syslog_value_filter_button($hosts[$dm['host_id']] ?? __('Unknown', 'syslog'), 'host') . '</td>';
-							print "<td class='left'>" . syslog_value_filter_button($dm['program'], 'program') . '</td>';
+							print "<td class='nowrap left'>" . syslog_value_filter_button($hosts[$dm['host_id']] ?? __('Unknown', 'syslog'), 'host') . '</td>';
+							print "<td class='nowrap left'>" . syslog_value_filter_button($dm['program'], 'program') . '</td>';
 							print "<td class='left syslogMessage'>" . syslog_message_button($dm['message'], $hosts[$dm['host_id']] ?? '', $dm['program'], $facilities[$dm['facility_id']] ?? '', $priorities[$dm['priority_id']] ?? '', $dm['logtime'], $dm[$syslog_incoming_config['id']], $sm['mtype']) . '</td>';
 							print "<td class='left'>" . syslog_metadata_label(isset($facilities[$dm['facility_id']]) ? $facilities[$dm['facility_id']] : __('Unknown', 'syslog'), 'facility') . '</td>';
-							print "<td class='left'>" . syslog_metadata_label(isset($priorities[$dm['priority_id']]) ? $priorities[$dm['priority_id']] : __('Unknown', 'syslog'), 'priority') . '</td>';
+							print "<td class='left'>" . syslog_value_filter_button($priorities[$dm['priority_id']] ?? __('Unknown', 'syslog'), 'priority') . '</td>';
 
 							if ($grouping_enabled) {
 								print "<td class='right'></td>";
@@ -2012,9 +2012,9 @@ function syslog_messages($tab = 'syslog') {
 				form_selectable_cell(syslog_message_button($log['logmsg'], $log['host'], $log['program'] ?? '', $log['facility'], $log['priority'], $log['logtime']), $log['seq'], '', 'syslogMessage left');
 
 				form_selectable_cell($log['count'], $log['seq'], '', 'right');
-				form_selectable_cell(syslog_value_filter_button($log['host'], 'host'), $log['seq'], '', 'right');
+				print "<td class='nowrap right'>" . syslog_value_filter_button($log['host'], 'host') . '</td>';
 				form_selectable_cell(ucfirst($log['facility']), $log['seq'], '', 'right');
-				form_selectable_cell(ucfirst($log['priority']), $log['seq'], '', 'right');
+				print "<td class='nowrap right'>" . syslog_value_filter_button(ucfirst($log['priority']), 'priority') . '</td>';
 
 				form_end_row();
 			}
